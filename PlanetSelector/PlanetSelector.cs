ae7c8d00b4d114f4bbf571e80bd4400552e18908
Philip Woelfel <philip@woelfel.at>
2014-10-08 18:07:27 +0200

    initial commit

---
+++ PlanetSelector/PlanetSelector.cs
@@ -0,0 +1,47 @@
+﻿using PluginBaseFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PlanetSelector
+{
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+    class PlanetSelector : PluginBase
+    {
+		private IButton psButton;
+
+		private PlanetSelectorWindow psWindow;
+
+		public void Start()
+		{
+			Log("start");
+			Settings.load();
+
+			psWindow = gameObject.AddComponent<PlanetSelectorWindow>();
+
+			if (ToolbarManager.ToolbarAvailable)
+			{
+				LogDebug("add toolbar button");
+				psButton = ToolbarManager.Instance.add("PS", "PSButton");
+				psButton.TexturePath = "PlanetSelector/icons/ps_toolbar_button";
+				psButton.ToolTip = "Planet Selector Window";
+				psButton.OnClick += (e) =>
+				{
+					psWindow.windowVisible = !psWindow.windowVisible;
+				};
+			}
+		}
+		
+		void OnDestroy()
+		{
+			Log("destroy");
+
+			if (ToolbarManager.ToolbarAvailable)
+			{
+				psButton.Destroy();
+			}
+		}
+    }
+}
---
+++ PlanetSelector/PlanetSelectorWindow.cs
@@ -0,0 +1,76 @@
+﻿using PluginBaseFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PlanetSelector
+{
+	class PlanetSelectorWindow : BaseWindow
+	{
+		private Dictionary<String, int> planets = new Dictionary<String, int>();
+		private PlanetariumCamera pCam;
+
+		public PlanetSelectorWindow()
+			: base("Planet Selector", 200, 400)
+		{
+
+		}
+
+		public void Start()
+		{
+			LogDebug("window start");
+			windowPosition.x = KSPSettings.get("PlanetSelectorWindow.x", (int)(Screen.width / 2 - windowWidth / 2));
+			windowPosition.y = KSPSettings.get("PlanetSelectorWindow.y", (int)(Screen.height / 2 - windowHeight / 2));
+
+			pCam = MapView.MapCamera;
+			List<MapObject> targets = pCam.targets;
+
+			foreach (MapObject mo in targets)
+			{
+				LogDebug("MapObject: " + mo.GetName() + " type: " + mo.type);
+
+			}
+		}
+
+		protected override void preDrawGui()
+		{
+
+		}
+
+		protected override void drawGui(int windowID)
+		{
+			GUILayout.BeginVertical();
+
+			foreach (MapObject mo in pCam.targets)
+			{
+				if (GUILayout.Button(mo.GetName()))
+				{
+					if (!MapView.MapIsEnabled)
+					{
+						MapView.EnterMapView();
+					}
+					pCam.SetTarget(mo);
+				}
+			}
+			
+			GUILayout.EndVertical();
+			GUI.DragWindow();
+		}
+
+		public void OnDestroy()
+		{
+			LogDebug("PlanetSelectorWindow destroy");
+			saveSettings();
+		}
+
+		protected void saveSettings()
+		{
+			KSPSettings.set("PlanetSelectorWindow.x", (int)windowPosition.x);
+			KSPSettings.set("PlanetSelectorWindow.y", (int)windowPosition.y);
+
+			KSPSettings.save();
+		}
+	}
+}
